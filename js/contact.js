8d2dce92d593248b355b747f706de5605f026c9b
Kevin Legendre <kevinlegendre.dev@gmail.com>
2021-04-14 19:49:14 +0200

    Add change contact

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,12 @@
-sendEmail = function () {
+sendEmail = function (name, email, message) {
+    let constructMessage = name + "\n" + email + "\n" + message
     Email.send({
-        Host: "smtp.gmail.com.",
-        Username: "kevinlegendre.dev@gmail.com",
-        Password: "BravoTuAsTrouve34590",
-        To: 'kevin.legendre705@gmail.com',
+        SecureToken: "912383e2-5358-4b1a-880d-7c7efa193e9f",
+        To: 'kevinlegendre.dev@gmail.com',
         From: "kevinlegendre.dev@gmail.com",
-        Subject: "This is the subject",
-        Body: "And this is the body"
-        }).then(
-        message => alert(message)
-    );
+        Subject: "Contact depuis mon site",
+        Body: constructMessage,
+    }).then(
+        message => alert(message));
+
 }
